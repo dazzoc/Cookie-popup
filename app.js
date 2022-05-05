8299b7943aaa986b6822bc2a508c1e6c214dc53b
Christopher Dazzo <dazzo.chris@gmail.com>
2022-05-05 10:44:28 -0500

    cookie done

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ tl.fromTo('.cookie', {opacity: 0, x: -50, rotation: '-45deg'}, {opacity: 1, x: 0
 tl.fromTo('.text', {x: 30, opacity: 0}, {x: 0, opacity: 1}, '<');
 
 //Crumb animation ======================================================================== 
-tl.fromTo('.cookie', {y: 10, rotation: '0deg'}, {y: -10, rotation: '-20deg', yoyo: true, repeat: -1})
-tl.fromTo('#crumbs', {y: 10}, {y: -10, yoyo: true, repeat: -1}, '<')
+tl.fromTo('.cookie', {y: 10, rotation: '0deg'}, {y: -10, rotation: '-20deg', yoyo: true, repeat: -1});
+tl.fromTo('#crumbs', {y: 10}, {y: -10, yoyo: true, repeat: -1}, '<');
 
 // click to close =========================================================================
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    gsap.to('.cookie-container', {opacity: 0, y: 100, duration: 0.75, ease: 'power1.out'});
-})
+    gsap.to('.cookie-container', {opacity: 0, scale: 0.5, duration: 0.5, ease: 'power1.out'});
+});
 
