4ceeb6c28654b9c4bd89d2b80dacaa2042ca4896
Christopher Dazzo <dazzo.chris@gmail.com>
2022-05-05 09:26:23 -0500

    crumbs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-// Load Animation
+// Load Animation =======================================================================
 const tl = gsap.timeline({defaults: {duration: 0.75, ease: 'power1.out'}});
 
 tl.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: 'elastic.out(1, 0.4)', duration: 1.5});
 tl.fromTo('.cookie', {opacity: 0, x: -50, rotation: '-45deg'}, {opacity: 1, x: 0, rotation: '0deg'}, '<30%');
 tl.fromTo('.text', {x: 30, opacity: 0}, {x: 0, opacity: 1}, '<');
 
-// click to close
+//Crumb animation ======================================================================== 
+tl.fromTo('.cookie', {y: 10, rotation: '0deg'}, {y: -10, rotation: '-20deg', yoyo: true, repeat: -1})
+tl.fromTo('#crumbs', {y: 10}, {y: -10, yoyo: true, repeat: -1}, '<')
+
+// click to close =========================================================================
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
