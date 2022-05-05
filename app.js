e23717009bc2f48ea2b367c81aabf4d7b5ef7944
Christopher Dazzo <dazzo.chris@gmail.com>
2022-05-05 08:46:15 -0500

    cookie animation

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-// gsap animations 
-gsap.fromTo('.text', {opacity: 0, y: 30}, {opacity: 1, y: 0, duration: 1});
+// Load Animation
+const tl = gsap.timeline({defaults: {duration: 0.75, ease: 'power1.out'}});
+
+tl.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: 'elastic.out(1, 0.4)', duration: 1.5});
+tl.fromTo('.cookie', {opacity: 0, x: -50, rotation: '-45deg'}, {opacity: 1, x: 0, rotation: '0deg'}, '<30%');
+tl.fromTo('.text', {x: 30, opacity: 0}, {x: 0, opacity: 1}, '<');
+
+// click to close
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    gsap.to('.cookie-container', {opacity: 0, y: 100, duration: 0.75, ease: 'power1.out'});
+})
+
